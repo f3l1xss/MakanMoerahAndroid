6a789686ebca127de9992e0358bf3e21af9227c8
felix <25915239+f3l1xss@users.noreply.github.com>
2021-10-19 17:33:54 +0800

    [Reformat Code]

--- app/src/main/java/com/felixstanley/makanmoerahandroid/MainActivity.kt
+++ app/src/main/java/com/felixstanley/makanmoerahandroid/MainActivity.kt
@@ -9,7 +9,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         // Setup Bottom Navigation With Nav Controller
