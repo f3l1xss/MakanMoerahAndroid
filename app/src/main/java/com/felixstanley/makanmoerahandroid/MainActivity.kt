a7cd3b75136f69c4588e4b145eb75a4a635a723a
felix <25915239+f3l1xss@users.noreply.github.com>
2021-10-19 17:25:39 +0800

    Add Bottom Navigation with Fragment Navigation & Navigation Graph, Add Bottom Navigation Icon, Add Navigation Fragment & Navigation UI Library

--- app/src/main/java/com/felixstanley/makanmoerahandroid/MainActivity.kt
+++ app/src/main/java/com/felixstanley/makanmoerahandroid/MainActivity.kt
@@ -2,10 +2,25 @@ package com.felixstanley.makanmoerahandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        // Setup Bottom Navigation With Nav Controller
+        setupBottomNavigationWithNavController()
+    }
+
+    private fun setupBottomNavigationWithNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            .setupWithNavController(navController)
     }
 }
