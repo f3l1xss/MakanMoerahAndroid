d27faaaa22900b40cd61beec09525e57eea45f40
felix <25915239+f3l1xss@users.noreply.github.com>
2021-12-06 10:59:40 +0800

    Add Sort Radio Group, NumOfPeople Dropdown, Date & Time Edit Text, Price & Discount & Rating Slider to Explore Fragment Bottom Sheet

--- app/src/main/java/com/felixstanley/makanmoerahandroid/constants/Constants.kt
+++ app/src/main/java/com/felixstanley/makanmoerahandroid/constants/Constants.kt
@@ -6,4 +6,28 @@ object Constants {
 
     // Component Height
     const val RESTAURANT_CARD_CAROUSEL_HEIGHT_DP = 440
+
+    // Explore Fragment Number Of People Dropdown Items
+    val EXPLORE_FRAGMENT_NUM_OF_PEOPLE_DROPDOWN_ITEMS = listOf(
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20"
+    )
 }
--- app/src/main/java/com/felixstanley/makanmoerahandroid/fragment/explore/ExploreFragment.kt
+++ app/src/main/java/com/felixstanley/makanmoerahandroid/fragment/explore/ExploreFragment.kt
@@ -1,15 +1,23 @@
 package com.felixstanley.makanmoerahandroid.fragment.explore
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.felixstanley.makanmoerahandroid.R
+import com.felixstanley.makanmoerahandroid.constants.Constants
 import com.felixstanley.makanmoerahandroid.databinding.FragmentExploreBinding
 import com.felixstanley.makanmoerahandroid.fragment.AbstractFragment
 import com.felixstanley.makanmoerahandroid.network.api.NetworkApi
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import java.text.SimpleDateFormat
+import java.time.LocalTime
+import java.util.*
 
 class ExploreFragment : AbstractFragment() {
 
@@ -17,6 +25,15 @@ class ExploreFragment : AbstractFragment() {
     private lateinit var viewModelFactory: ExploreViewModelFactory
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private lateinit var restaurantFilterBottomSheetDateEditText: EditText
+    private lateinit var restaurantFilterBottomSheetTimeEditText: EditText
+
+    // Stores Latest Value of Date selected via Date Edit Text
+    private val dateEditTextCalendar = Calendar.getInstance()
+    private val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+
+    // Stores Latest Value of Time selected via Time Edit Text
+    private var timeEditTextTime = LocalTime.now()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -111,9 +128,108 @@ class ExploreFragment : AbstractFragment() {
             foodCategoryFilterCheckboxAdapter.addList(it)
         })
 
+        // Adjust Price Slider Label Formatter
+        val priceSlider = binding.restaurantFilterBottomSheetPriceSlider
+        priceSlider.setLabelFormatter { labelValue ->
+            when (labelValue.toInt()) {
+                1 -> "$"
+                2 -> "$$"
+                3 -> "$$$"
+                4 -> "$$$$"
+                5 -> "$$$$$"
+                else -> "$$$$$"
+            }
+        }
+
+        // Adjust Discount Slider Label Formatter
+        val discountSlider = binding.restaurantFilterBottomSheetDiscountSlider
+        discountSlider.setLabelFormatter { labelValue ->
+            "${labelValue.toInt()}%"
+        }
+
+        // Adjust Rating Slider Label Formatter
+        val ratingSlider = binding.restaurantFilterBottomSheetRatingSlider
+        ratingSlider.setLabelFormatter { labelValue ->
+            when (labelValue.toInt()) {
+                1 -> "★"
+                2 -> "★★"
+                3 -> "★★★"
+                4 -> "★★★★"
+                5 -> "★★★★★"
+                else -> "★★★★★"
+            }
+        }
+
+        // Initialize Number Of People Exposed Dropdown List Item
+        val numberOfPeopleItemAdapter =
+            ArrayAdapter(
+                requireContext(),
+                R.layout.num_of_people_list_item,
+                Constants.EXPLORE_FRAGMENT_NUM_OF_PEOPLE_DROPDOWN_ITEMS
+            )
+        val restaurantFilterBottomSheetNumOfPeopleTextView =
+            binding.restaurantFilterBottomSheetNumOfPeopleTextView
+        restaurantFilterBottomSheetNumOfPeopleTextView.setAdapter(numberOfPeopleItemAdapter)
+        // Set Default Text
+        restaurantFilterBottomSheetNumOfPeopleTextView.setText("2", false)
+
+        // Initialize Date Edit Text to show Date Picker Dialog Upon Click
+        restaurantFilterBottomSheetDateEditText = binding.restaurantFilterBottomSheetDateEditText
+        val datePickerDialogListener =
+            DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                dateEditTextCalendar.set(year, month, day)
+                updateDateEditText()
+            }
+        restaurantFilterBottomSheetDateEditText.setOnClickListener { it ->
+            // Show Date Picker Dialog Upon Edit Text Click
+            DatePickerDialog(
+                requireContext(),
+                datePickerDialogListener,
+                dateEditTextCalendar.get(Calendar.YEAR),
+                dateEditTextCalendar.get(Calendar.MONTH),
+                dateEditTextCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+        // Update Date Edit Text Once to set Text as current Date
+        updateDateEditText()
+
+        // Initialize Time Edit Text to Show Time Picker Dialog Upon Click
+        restaurantFilterBottomSheetTimeEditText = binding.restaurantFilterBottomSheetTimeEditText
+        val timePickerDialogListener = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+            timeEditTextTime = timeEditTextTime.withHour(hour).withMinute(minute)
+            updateTimeEditText()
+        }
+        restaurantFilterBottomSheetTimeEditText.setOnClickListener { it ->
+            // Show Time Picker Dialog Upon Edit Text Click
+            TimePickerDialog(
+                requireContext(),
+                timePickerDialogListener,
+                timeEditTextTime.hour,
+                timeEditTextTime.minute,
+                true
+            ).show()
+        }
+        // Update Time Edit Text Once to set Text as current Time
+        updateTimeEditText()
+
         // Initialize Options Menu
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    private fun updateDateEditText() {
+        // Update Date Edit Text Content to current value of dateEditTextCalendar
+        restaurantFilterBottomSheetDateEditText.setText(dateFormat.format(dateEditTextCalendar.time))
+    }
+
+    private fun updateTimeEditText() {
+        // Prepend with 0 if either hour or minute consist only of single digit
+        val hour =
+            if (timeEditTextTime.hour >= 10) "${timeEditTextTime.hour}" else "0${timeEditTextTime.hour}"
+        val minute =
+            if (timeEditTextTime.minute >= 10) "${timeEditTextTime.minute}" else "0${timeEditTextTime.minute}"
+        // Update Time Edit Text Content to current value of timeEditTextTime
+        restaurantFilterBottomSheetTimeEditText.setText("$hour:$minute")
     }
 
     private fun expandBottomSheet() {
